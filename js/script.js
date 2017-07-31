cf53ac79d84b8e0e142b1e856a46a9cd9a75797d
andregama <eng.andregama@gmail.com>
2017-07-31 19:21:46 -0300

    Correções no CSS e primeiro javascript

---
+++ js/script.js
@@ -0,0 +1,13 @@
+//var resposta = confirm("Deseja excluir?");
+
+// alert("Olá, HCODE!");
+//console.log(resposta);
+//var a = 10;
+//var b = 33;
+//alert(a+b);
+
+function somar (a, b){
+
+return (a+b);
+}
+console.log(somar(2, 5));
