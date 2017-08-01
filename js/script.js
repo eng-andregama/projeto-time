8cae129f34988f990824bf3512837c54438e18ea
andregama <eng.andregama@gmail.com>
2017-08-01 01:22:39 -0300

    Adicionados arquivos bootstrap e melhorias no js

--- js/script.js
+++ js/script.js
@@ -2,12 +2,22 @@
 
 // alert("Olá, HCODE!");
 //console.log(resposta);
-//var a = 10;
-//var b = 33;
+const a = 10;
+const b = 33;
 //alert(a+b);
 
-function somar (a, b){
-
-return (a+b);
+function somar(a, b){
+  return parseInt(a)+ parseInt(b);
 }
-console.log(somar(2, 5));
+/*console.log(somar(2, 5));
+*/
+document.querySelector("#calcular").addEventListener("click",function() {
+let valorA = document.querySelector("#valorA").value;
+let valorB = document.querySelector("#valorB").value;
+if (valorA.length > 0 && valorB.length > 0){
+    //alert(parseInt(valorA) + parseInt(valorB));
+    alert (somar(valorA, valorB));
+}else{
+    alert("Digite os valores para o cálculo");
+}
+});
